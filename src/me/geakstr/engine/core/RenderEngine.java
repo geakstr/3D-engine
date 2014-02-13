b5a5f682eb72f43893993e02fa9aae7fb5e4d8b1
geakstr <geakstr@me.com>
2014-02-13 20:44:24 +0400

    Added RenderEngine template

---
+++ src/me/geakstr/engine/core/RenderEngine.java
@@ -0,0 +1,13 @@
+package me.geakstr.engine.core;
+
+public class RenderEngine {
+
+    public static void renderModel(int model) {
+
+    }
+
+    public static void renderBlock(int block) {
+
+    }
+
+}
