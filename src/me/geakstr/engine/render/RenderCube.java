2c75c370fd9cc3d31606b3a8dfdd0ca227476813
geakstr <geakstr@me.com>
2014-02-24 21:30:40 +0400

    Refactoring, naming, improvements

--- src/me/geakstr/engine/render/RenderCube.java
+++ src/me/geakstr/engine/render/RenderCube.java
@@ -1,10 +1,7 @@
 package me.geakstr.engine.render;
 
-import me.geakstr.engine.core.Shader;
-import me.geakstr.engine.core.Transform;
 import me.geakstr.engine.core.World;
 import me.geakstr.engine.model.Cube.Cube;
-import me.geakstr.engine.model.Cube.CubeType;
 import me.geakstr.engine.model.ResourceBuffer;
 
 import org.lwjgl.BufferUtils;
@@ -13,16 +10,14 @@ import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL32.glDrawElementsBaseVertex;
 
 public class RenderCube {
     public static int indexSize;
 
-    public static void prepare() {
+    public static void buffering() {
         List<Float> vertices = new ArrayList<Float>();
         List<Float> normals = new ArrayList<Float>();
         List<Float> texCoords = new ArrayList<Float>();
@@ -32,18 +27,18 @@ public class RenderCube {
         for (int x = 0; x < World.getLength(); x++) {
             for (int y = 0; y < World.getHeight(); y++) {
                 for (int z = 0; z < World.getWidth(); z++) {
-                    CubeType[] sides = World.isRenderableSides(World.get(x, y, z), x, y, z);
+                    Cube.Side[] sides = World.isRenderableSides(World.get(x, y, z), x, y, z);
                     if (sides == null) continue;
-                    
-                    for (CubeType side : sides) {
+
+                    for (Cube.Side side : sides) {
 	                    for (int i = 0; i <side.vertices().length; i += 3) {
 	                        vertices.add(side.vertices()[i] + x);
 	                        vertices.add(side.vertices()[i + 1] + y);
 	                        vertices.add(side.vertices()[i + 2] + z);
 	                    }
 	                    for (int k = 0; k < 6; k++) {
-	                        for (int j = 0; j < 3; j++) {
-	                            normals.add(side.normals()[j]);
+                            for (int i = 0; i < side.normals().length; i++) {
+	                            normals.add(side.normals()[i]);
 	                        }
 	                    }
 	                    for (int i = 0; i < side.texCoords().length; i += 2) {
@@ -56,11 +51,11 @@ public class RenderCube {
             }
         }
         indexSize *= 6;
-        
+
         // Fill buffers
         IntBuffer indexBuffer = BufferUtils.createIntBuffer(indexSize);
         for (int i = 0; i < indexSize; i++) indexBuffer.put(i);
-        
+
         int size = vertices.size();
         FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(size);
         FloatBuffer normalBuffer = BufferUtils.createFloatBuffer(size);
@@ -69,13 +64,6 @@ public class RenderCube {
         	normalBuffer.put(normals.get(i));
         }
 
-        FloatBuffer colorBuffer = BufferUtils.createFloatBuffer(size);
-        for (int i = 0; i < indexSize; i++) {
-            for (int j = 0; j < 3; j++) {
-                colorBuffer.put(Cube.diffuse);
-            }
-        }
-        
         size /= 1.5;
         FloatBuffer textureBuffer = BufferUtils.createFloatBuffer(size);
         for (int i = 0; i < size; i++) textureBuffer.put(texCoords.get(i));
@@ -84,36 +72,30 @@ public class RenderCube {
         indexBuffer.rewind();
         vertexBuffer.rewind();
         normalBuffer.rewind();
-        colorBuffer.rewind();
         textureBuffer.rewind();
 
 
-        // Gen buffers
-        final int vboIndex = glGenBuffers();
-        final int vboVertex = glGenBuffers();
-        final int vboNormal = glGenBuffers();
-        final int vboColor = glGenBuffers();
-        final int vboTexture = glGenBuffers();
+        // Gen ?BO
+        final int ibo = glGenBuffers();
+        final int vbo = glGenBuffers();
+        final int nbo = glGenBuffers();
+        final int tbo = glGenBuffers();
 
 
         // Assign data to buffers
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIndex);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexBuffer, GL_STATIC_DRAW);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vboVertex);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vboNormal);
+        glBindBuffer(GL_ARRAY_BUFFER, nbo);
         glBufferData(GL_ARRAY_BUFFER, normalBuffer, GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vboColor);
-        glBufferData(GL_ARRAY_BUFFER, colorBuffer, GL_STATIC_DRAW);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-        glBindBuffer(GL_ARRAY_BUFFER, vboTexture);
+        glBindBuffer(GL_ARRAY_BUFFER, tbo);
         glBufferData(GL_ARRAY_BUFFER, textureBuffer, GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -121,36 +103,28 @@ public class RenderCube {
         // Bind buffers
         glEnableClientState(GL_VERTEX_ARRAY);
         glEnableClientState(GL_NORMAL_ARRAY);
-        glEnableClientState(GL_COLOR_ARRAY);
-
         glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
         glBindTexture(GL_TEXTURE_2D, ResourceBuffer.getTexturesID(World.getCubeId()));
-        glBindBuffer(GL_ARRAY_BUFFER, vboTexture);
+        glBindBuffer(GL_ARRAY_BUFFER, tbo);
         glTexCoordPointer(2, GL_FLOAT, 0, 0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vboNormal);
+        glBindBuffer(GL_ARRAY_BUFFER, nbo);
         glNormalPointer(GL_FLOAT, 0, 0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vboColor);
-        glColorPointer(3, GL_FLOAT, 0, 0);
-
-        glBindBuffer(GL_ARRAY_BUFFER, vboVertex);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glVertexPointer(3, GL_FLOAT, 0, 0);
 
-        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIndex);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
     }
 
-    public static void render(Shader shader) {
-        Transform transform = new Transform();
-        shader.setUniform("mModelTransform", transform.getTransform());
-
-        glDrawElementsBaseVertex(GL_TRIANGLES, indexSize, GL_UNSIGNED_INT, 0, 0);
+    public static void render() {
+        glDrawElements(GL_TRIANGLES, indexSize, GL_UNSIGNED_INT, 0);
     }
 
     public static void dispose() {
-        glDisableClientState(GL_VERTEX_ARRAY);
-        glDisableClientState(GL_NORMAL_ARRAY);
-        glDisableClientState(GL_COLOR_ARRAY);
         glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+        glDisableClientState(GL_NORMAL_ARRAY);
+        glDisableClientState(GL_VERTEX_ARRAY);
     }
 }
